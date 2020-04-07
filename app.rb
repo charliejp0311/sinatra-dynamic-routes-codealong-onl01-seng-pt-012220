8eb87c80be8bc314b9668220935db41003d8cab0
Charles Pelton <charliejp0311@gmail.com>
2020-04-07 16:13:30 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -20,8 +20,9 @@ class App < Sinatra::Base
   end
 
   get "/multiply/:num1/:num2" do
-    @answer = params[:num1] * params[:num2]
-    "#{@answer}"
+    @num1 = params[:num1] 
+    @num2 = params[:num2]
+    "#{@num1 * @num2}"
   end
 
 
